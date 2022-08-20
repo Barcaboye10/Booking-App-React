869bba2bd2120b725a7edd5814cfe65d4f7c26e7
Barcaboye10 <rajgupta2140@gmail.com>
2022-08-20 14:27:13 +0530

    UI Done

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -1,18 +1,20 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
-
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/hotels" element={<List/>}/>
-      <Route path="/hotel" element={<Hotel/>}/>
-    </Routes>
-    </BrowserRouter>
+    <div className="appBody">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<List />} />
+          <Route path="/hotel" element={<Hotel />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
